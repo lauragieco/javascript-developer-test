c37dfd8a8d8ffdc4ffbd30722f163d76b5b40cfa
laura <lauragieco@protonmail.com>
2022-01-28 12:45:37 +1000

    Implement getArnieQuotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,23 @@
 const { httpGet } = require('./mock-http-interface');
 
-const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+const getArnieQuotes = async (urls = []) => {
+  const promises = [];
+
+  for (const url of urls) {
+    promises.push(
+      httpGet(url).then((response) => {
+        const message = JSON.parse(response.body).message || '';
+
+        return {
+          [response.status === 200 ? 'Arnie Quote' : 'FAILURE']: message,
+        };
+      })
+    );
+  }
+
+  return Promise.allSettled(promises).then((results) =>
+    results.map((result) => result.value)
+  );
 };
 
 module.exports = {
